d20bef0f571df75f273c541d0b943853f661d0a9
thisisommore <ommore501@gmail.com>
2026-04-01 13:37:09 +0530

    Lower minSdk from 36 to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.haven"
-        minSdk = 36
+        minSdk = 33
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
