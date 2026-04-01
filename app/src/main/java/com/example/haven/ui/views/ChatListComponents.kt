77d388a75bbd936127674cb4f5a42cea52e5d93a
thisisommore <ommore501@gmail.com>
2026-04-01 10:35:45 +0530

    feat: change chat item height to 90dp

--- app/src/main/java/com/example/haven/ui/views/ChatListComponents.kt
+++ app/src/main/java/com/example/haven/ui/views/ChatListComponents.kt
@@ -46,7 +46,7 @@ fun ChatListItem(
         Row(
             modifier = modifier
                 .fillMaxWidth()
-                .height(120.dp)
+                .height(90.dp)
                 .clickable(onClick = onClick)
                 .padding(horizontal = 16.dp),
             verticalAlignment = Alignment.CenterVertically,
@@ -141,7 +141,7 @@ fun ChatListItem(
         ),
         modifier = modifier
             .fillMaxWidth()
-            .height(120.dp)
+            .height(90.dp)
             .clickable(onClick = onClick)
     )
 }
